0e6f6853a9b1cb5faa3fae7db9c104260bc02805
Mauricio <mauriciorg89@hotmail.com>
2024-05-17 19:37:22 -0300

    Conexao com bd

---
+++ src/entities/BaseDeDados.java
@@ -0,0 +1,21 @@
+package entities;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class BaseDeDados {
+	
+
+	  public static void main(String[] args) {
+		  
+	        try {
+	            // Obtém uma conexão com o banco de dados
+	            Connection connection = DatabaseConnection.getConnection();
+	            System.out.println("Conexão bem-sucedida ao banco de dados.");
+	            // Fecha a conexão
+	            connection.close();
+	        } catch (SQLException e) {
+	            System.err.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+	        }
+	    }
+	}
